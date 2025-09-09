18b0c932429e7d8e7d737698cb2969f1b8e50b7c
CelioVilela <celiocvilela70@gmail.com>
2025-09-09 11:20:57 -0300

    aula_09_exercicio_02.js

--- aula_09_exercicio_02.js
+++ aula_09_exercicio_02.js
@@ -1,1 +1,6 @@
+// Ler um número inteiro e apresentar o resultado do quadrado desse número
+const readline = require('readline-sync');
+let num = readline.question('Insira o número: ');
+let quadrado = (num * num);
 
+console.log("O resultado é: ", quadrado);
